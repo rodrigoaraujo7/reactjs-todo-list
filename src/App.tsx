25f9bebfa0b2802dd53a4d11c21ce702e3215f93
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-16 16:20:51 -0300

    globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from './globalStyles'
+
 const App = () => {
-  return <h1>Hello World!</h1>
+  return (
+    <div>
+      <GlobalStyle />
+      <h1>Hello World!</h1>
+    </div>
+  )
 }
 
 export default App

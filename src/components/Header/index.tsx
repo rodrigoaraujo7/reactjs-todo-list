b41a24acf42eaf3eb7a6feb104cac0f2f7701d1a
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-16 17:54:18 -0300

    install fontAwesome

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,13 +6,17 @@ import {
     InputButton 
 } from "./style";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
 const Header = () => {
     return (
         <Container>
             <Title>what will you do <BoldTitle>today?</BoldTitle></Title>
             <InputContainer>
                 <InputText type="text" placeholder="place here!" />
-                <InputButton>V</InputButton>
+                <InputButton><FontAwesomeIcon icon={faCheck as IconProp} /></InputButton>
             </InputContainer>
         </Container>
     )

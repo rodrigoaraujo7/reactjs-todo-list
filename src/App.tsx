757974ef6b88debf01b64bdbe9fa8b0d8ac4e3eb
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2022-12-17 15:48:33 -0300

    refatoring all code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,70 @@
 import { useState } from 'react'
 
-import Header from './components/Header'
-import { Container, GlobalStyle } from './globalStyles'
+import { Container, GlobalStyle } from './globalStyles';
+import { 
+  BoldTitle, 
+  Title, 
+  HeaderContainer, 
+  InputContainer, 
+  InputText, 
+  InputButton,
+  TodoItemContainer,
+  TodoItemTitle
+} from "./style";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // fontAwesome
+import { IconProp } from '@fortawesome/fontawesome-svg-core'; // fontAwesome Props
+import { 
+  faCheck, 
+  faSquareCheck, 
+  faTrashCan 
+} from '@fortawesome/free-solid-svg-icons'; // fontAwesome Icons
 
 const App = () => {
+  // read input-text value!
+  const [itemName, setItemName] = useState();
+  const getItemName = (event: any) => setItemName(event.target.value);
+
+  // create new todo-item
+  const [todoItem, setTodoItem] = useState<any[]>([]);
+  const createNewTodoItem = () => {
+      setTodoItem([itemName, ...todoItem])
+  }
+
+  // check item
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckItem = () => {
+      setIsChecked(current => !current)
+  }
+
   return (
     <Container>
       <GlobalStyle />
-      <Header />
+
+      {/* Header */}
+      <HeaderContainer>
+            <Title>what will you do <BoldTitle>today?</BoldTitle></Title>
+            <InputContainer>
+                <InputText type="text" placeholder="place here!" onChange={getItemName} value={itemName} />
+                <InputButton onClick={createNewTodoItem}>
+                    <FontAwesomeIcon icon={faCheck as IconProp} />
+                </InputButton>
+            </InputContainer>
+        </HeaderContainer>
+
+        {/* Todo List */}
+        <HeaderContainer style={{ height: '70%' }}>
+            {todoItem.map(item => (
+              <TodoItemContainer>
+                <FontAwesomeIcon icon={faSquareCheck as IconProp} onClick={handleCheckItem} />
+                <TodoItemTitle style={{
+                    textDecoration: isChecked ? 'line-through' : 'none',
+                    color: isChecked ? '#B0B0B0' : '#808080'
+                }}>{item}</TodoItemTitle>
+              </TodoItemContainer> 
+            ))}
+        </HeaderContainer>
     </Container>
   )
 }
--- src/style.ts
+++ src/style.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const HeaderContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -80,4 +80,27 @@ export const InputButton = styled.button`
     align-items: center;
 
     cursor: pointer;
+`
+
+export const TodoItemContainer = styled.div`
+    display: flex;
+    align-items: center;
+
+    width: 100%;
+    height: 85px;
+
+    padding: 0 15px 0;
+    margin-top: 15px;
+
+    background-color: #F1F1F1;
+    box-shadow: 0 2.5px 10px #3C3C3C35;
+
+    color: #808080;
+`
+
+export const TodoItemTitle = styled.h2`
+    font-size: 20px;
+    font-weight: 500;
+
+    margin-left: 10px;
 `
